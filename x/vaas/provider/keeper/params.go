a852de1f9db47da95c52c6d70a9f33108f33a879
julienrbrt <julien@rbrt.fr>
2026-02-06 21:43:09 +0100

    refactor: migrate to collections (#12)
    * refactor: migrate to collections
    
    * use collections indexes
    
    * lint
    
    * use paginate
    
    * fix conflicts

--- x/vaas/provider/keeper/params.go
+++ x/vaas/provider/keeper/params.go
@@ -1,63 +1,59 @@
 package keeper
 
 import (
+	"context"
 	"fmt"
 	"time"
 
 	"github.com/allinbits/vaas/x/vaas/provider/types"
 
 	ibctmtypes "github.com/cosmos/ibc-go/v10/modules/light-clients/07-tendermint"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // GetTemplateClient returns the template consumer client
-func (k Keeper) GetTemplateClient(ctx sdk.Context) *ibctmtypes.ClientState {
+func (k Keeper) GetTemplateClient(ctx context.Context) *ibctmtypes.ClientState {
 	params := k.GetParams(ctx)
 	return params.TemplateClient
 }
 
 // GetTrustingPeriodFraction returns a TrustingPeriodFraction
 // used to compute the provider IBC client's TrustingPeriod as UnbondingPeriod / TrustingPeriodFraction
-func (k Keeper) GetTrustingPeriodFraction(ctx sdk.Context) string {
+func (k Keeper) GetTrustingPeriodFraction(ctx context.Context) string {
 	params := k.GetParams(ctx)
 	return params.TrustingPeriodFraction
 }
 
 // GetVAASTimeoutPeriod returns the timeout period for sent ibc packets
-func (k Keeper) GetVAASTimeoutPeriod(ctx sdk.Context) time.Duration {
+func (k Keeper) GetVAASTimeoutPeriod(ctx context.Context) time.Duration {
 	params := k.GetParams(ctx)
 	return params.VaasTimeoutPeriod
 }
 
 // GetBlocksPerEpoch returns the number of blocks that constitute an epoch
-func (k Keeper) GetBlocksPerEpoch(ctx sdk.Context) int64 {
+func (k Keeper) GetBlocksPerEpoch(ctx context.Context) int64 {
 	params := k.GetParams(ctx)
 	return params.BlocksPerEpoch
 }
 
 // GetMaxProviderConsensusValidators returns the number of validators that will be passed on from the staking module
 // to the consensus engine on the provider
-func (k Keeper) GetMaxProviderConsensusValidators(ctx sdk.Context) int64 {
+func (k Keeper) GetMaxProviderConsensusValidators(ctx context.Context) int64 {
 	params := k.GetParams(ctx)
 	return params.MaxProviderConsensusValidators
 }
 
 // GetParams returns the paramset for the provider module
-func (k Keeper) GetParams(ctx sdk.Context) types.Params {
-	store := ctx.KVStore(k.storeKey)
-	bz := store.Get(types.ParametersKey())
-	var params types.Params
-	err := k.cdc.Unmarshal(bz, &params)
+func (k Keeper) GetParams(ctx context.Context) types.Params {
+	params, err := k.Params.Get(ctx)
 	if err != nil {
-		panic(fmt.Sprintf("error unmarshalling module parameters: %v:", err))
+		panic(fmt.Sprintf("error getting module parameters: %v", err))
 	}
 	return params
 }
 
 // SetParams sets the params for the provider module
-func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
-	store := ctx.KVStore(k.storeKey)
-	bz := k.cdc.MustMarshal(&params)
-	store.Set(types.ParametersKey(), bz)
+func (k Keeper) SetParams(ctx context.Context, params types.Params) {
+	if err := k.Params.Set(ctx, params); err != nil {
+		panic(fmt.Sprintf("error setting module parameters: %v", err))
+	}
 }
